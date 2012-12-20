9e85c099e33a18129e58cc5b7d20b9c5f17efd18
aude <aude.wiki@gmail.com>
2012-12-20 02:40:53 +0000

    localisation cache settings

--- CommonSettings.php
+++ CommonSettings.php
@@ -48,6 +48,10 @@ $wgDBmysql5 = false;
 $wgMainCacheType    = CACHE_MEMCACHED;
 $wgMemCachedServers = array( '127.0.0.1:11211' );
 
+## Localisation cache
+$wgLocalisationCacheConf['storeDirectory'] = "$IP/cache/l10n";
+$wgLocalisationCacheConf['manualRecache'] = true;
+
 ## To enable image uploads, make sure the 'images' directory
 ## is writable, then set this to true:
 $wgEnableUploads  = true;
